0a436906bca0b17191e5c148905fdebf32f082f8
lzmunch <lauren.zll.zhang@gmail.com>
2020-08-08 15:49:27 -0400

    progress 1

---
+++ src/components/hline.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class hline extends Component {
+  render() {
+    return (
+	    <hr
+	    	style={{
+	    		margin: 20
+	    	}}
+	    />
+    );
+  }
+}
+
+export default hline;
