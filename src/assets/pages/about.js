815614b2fa42701d4f9abbb2f6aeae614776b3a3
lzmunch <lauren.zll.zhang@gmail.com>
2020-09-14 16:05:32 -0400

    updated about to link to repo for website

--- src/assets/pages/about.js
+++ src/assets/pages/about.js
@@ -13,6 +13,8 @@ const project = () => (
 	  <br/>
 	  <p>Outside of "work" I like to make a variety of 2D art, mostly doodling and worldbuilding. Sometimes I write poetry too. I really like children's media, and anything that is weird and funny. Things that challenge you, but have a sense of humor and playfulness about it.</p>
 	  <br/>
+	  <p>I created this website by refactoring and building out a template website with React. The Github repo can be found <a href="https://github.com/lzmunch/lzmunch.github.io" target="_blank" rel="noopener noreferrer">here</a>.</p>
+	  <br/>
 	  <a href="https://linkedin.com/in/lauzhang" target="_blank" rel="noopener noreferrer">LinkedIn</a><br/>
   	  <a href="https://github.com/lzmunch" target="_blank" rel="noopener noreferrer">Github</a><br/>
 	  <a href="https://instagram.com/lz_munch" target="_blank" rel="noopener noreferrer">Instagram - @lz_munch</a><br/>
