2ac1937d3cd012c8323ade56b0e4ed7ff538670d
lzmunch <lauren.zll.zhang@gmail.com>
2021-06-28 19:30:18 -0700

    edit about page

--- src/assets/pages/about.js
+++ src/assets/pages/about.js
@@ -7,10 +7,9 @@ const project = () => (
 	  <img src={require("../about.jpg")} alt="me" style={{maxHeight:"300px"}}/>
 	  <h2>Software Developer + Creative</h2>
 	  <h3><a href={resumeLink} target="_blank" rel="noopener noreferrer">[Resume]</a></h3>
-	  <p style={{textAlign:"center"}}>WIP migration from <a href="https://zhauren.wixsite.com/home">https://zhauren.wixsite.com/home</a></p>
 	  <br/>
 
-	  <p>Hi! I'm an master's student at the Carnegie Mellon University Entertainment Technology Center, and an aspiring technical director/artist. My undergrad is in Computer Science at CMU. Much of my experience previous has been in creative technology and web development, but I'm hoping to explore some new areas. My current interests include technical direction, internet art, and physical spaces (especially themed entertainment).</p>
+	  <p>Hi! I am a 2nd year masters student at the Carnegie Mellon University Entertainment Technology Center, and an aspiring technical director/artist. My undergraduate degree is in Computer Science at CMU. Much of my experience previous has been in creative technology and web development, but I am working on becoming more familiar with animation, film, and game pipelines.</p>
 	  <br/>
 	  <p>Outside of "work" I like to make a variety of 2D art, mostly doodling and worldbuilding. Sometimes I write poetry too. I really like children's media, and anything that is weird and funny--things that challenge you, but have a sense of humor and playfulness about it.</p>
 	  <br/>
